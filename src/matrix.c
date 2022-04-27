69f7eb9072d5b9db88f995db9c512edf51bb658d
Eunice Lee <lalicorne@berkeley.edu>
2022-04-27 07:57:45 -0700

    check speed

--- src/matrix.c
+++ src/matrix.c
@@ -357,11 +357,11 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                 mul_vector = _mm256_fmadd_pd(_mm256_loadu_pd(&mat1->data[num * i + k]), _mm256_loadu_pd(&trans->data[num * j + k]), mul_vector);
             }
             // mul_vector = _mm256_add_pd(_mm256_add_pd(_mm256_add_pd(_mm256_add_pd(mul_vector, mul_vector1), mul_vector2), mul_vector3), mul_vector4);
+            double temp[4] = {};
+            _mm256_storeu_pd(temp, mul_vector);
             for (int k = num / 4 * 4; k < num; k++) {
                 sum += mat1->data[num * i + k] * trans->data[num * j + k];
             }
-            double temp[4] = {};
-            _mm256_storeu_pd(temp, mul_vector);
             sum += temp[0] + temp[1] + temp[2] + temp[3];
             result->data[col * i + j] = sum;
         }
