b8bcbbc0adaf72e22c567c62e3ec3af97308e096
Eunice Lee <lalicorne@berkeley.edu>
2022-04-26 20:53:28 -0700

    trans speed

--- src/matrix.c
+++ src/matrix.c
@@ -336,7 +336,7 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             double sum = 0.0;
-            //__m256d mul_vector = _mm256_set1_pd(0.0);
+            __m256d mul_vector = _mm256_set1_pd(0.0);
             __m256d mul_vector1 = _mm256_set1_pd(0.0);
             __m256d mul_vector2 = _mm256_set1_pd(0.0);
             __m256d mul_vector3 = _mm256_set1_pd(0.0);
@@ -348,16 +348,14 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                 mul_vector4 = _mm256_fmadd_pd(_mm256_loadu_pd(&mat1->data[num * i + k + 12]), _mm256_loadu_pd(&trans->data[num * j + k + 12]), mul_vector4);
             }
             for (int k = num / 16 * 16; k < num / 4 * 4; k += 4) {
-                // mul_vector = _mm256_fmadd_pd(_mm256_loadu_pd(&mat1->data[num * i + k]), _mm256_loadu_pd(&trans->data[num * j + k]), mul_vector);
-                mul_vector1 = _mm256_fmadd_pd(_mm256_loadu_pd(&mat1->data[num * i + k]), _mm256_loadu_pd(&trans->data[num * j + k]), mul_vector1);
+                mul_vector = _mm256_fmadd_pd(_mm256_loadu_pd(&mat1->data[num * i + k]), _mm256_loadu_pd(&trans->data[num * j + k]), mul_vector);
             }
-            // mul_vector = _mm256_add_pd(_mm256_add_pd(_mm256_add_pd(_mm256_add_pd(mul_vector, mul_vector1), mul_vector2), mul_vector3), mul_vector4);
-            mul_vector1 = _mm256_add_pd(_mm256_add_pd(_mm256_add_pd(mul_vector1, mul_vector2), mul_vector3), mul_vector4);
+            mul_vector = _mm256_add_pd(_mm256_add_pd(_mm256_add_pd(_mm256_add_pd(mul_vector, mul_vector1), mul_vector2), mul_vector3), mul_vector4);
             for (int k = num / 4 * 4; k < num; k++) {
                 sum += mat1->data[num * i + k] * trans->data[num * j + k];
             }
             double temp[4] = {};
-            _mm256_storeu_pd(temp, mul_vector1);
+            _mm256_storeu_pd(temp, mul_vector);
             sum += temp[0] + temp[1] + temp[2] + temp[3];
             result->data[col * i + j] = sum;
         }
@@ -378,6 +376,16 @@ matrix* trans_matrix(matrix *mat) {
     int col = mat->cols;
     #pragma omp parallel for
     for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col / 4 * 4; j += 4) {
+            trans->data[row * j + i] = mat->data[col * i + j];
+            trans->data[row * (j + 1) + i] = mat->data[col * i + j + 1];
+            trans->data[row * (j + 2) + i] = mat->data[col * i + j + 2];
+            trans->data[row * (j + 3) + i] = mat->data[col * i + j + 3];
+        }
+        for (int j = col / 4 * 4; j < col; j++) {
+            trans->data[row * j + i] = mat->data[col * i + j];
+        }
+        /*
         for (int j = 0; j < col / 16 * 16; j += 16) {
             trans->data[row * j + i] = mat->data[col * i + j];
             trans->data[row * (j + 1) + i] = mat->data[col * i + j + 1];
@@ -405,6 +413,7 @@ matrix* trans_matrix(matrix *mat) {
         for (int j = col / 4 * 4; j < col; j++) {
             trans->data[row * j + i] = mat->data[col * i + j];
         }
+        */
     }
     return trans;
 }
